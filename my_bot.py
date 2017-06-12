2863bbe641cfbe110d5957069c2289ebdc6e8ea3
dionysiusr77 <azt.razth@gmail.com>
2017-06-12 11:19:45 +0800

    Update my_bot.py

--- my_bot.py
+++ my_bot.py
@@ -1,36 +1,4 @@
-import requests  
-import datetime
-
-class BotHandler:
-
-    def __init__(self, token):
-        self.token = token
-        self.api_url = "https://api.telegram.org/bot{}/".format(token)
-
-    def get_updates(self, offset=None, timeout=30):
-        method = 'getUpdates'
-        params = {'timeout': timeout, 'offset': offset}
-        resp = requests.get(self.api_url + method, params)
-        result_json = resp.json()['result']
-        return result_json
-
-    def send_message(self, chat_id, text):
-        params = {'chat_id': chat_id, 'text': text}
-        method = 'sendMessage'
-        resp = requests.post(self.api_url + method, params)
-        return resp
-
-    def get_last_update(self):
-        get_result = self.get_updates()
-
-        if len(get_result) > 0:
-            last_update = get_result[-1]
-        else:
-            last_update = get_result[len(get_result)]
-
-        return last_update
-    
-    greet_bot = BotHandler('343975988:AAHQr4GW7akt6E5tvQOH3Q0cYim594BDkx4')  
+greet_bot = BotHandler('343975988:AAHQr4GW7akt6E5tvQOH3Q0cYim594BDkx4')  
 greetings = ('hello', 'hi', 'greetings', 'sup')  
 now = datetime.datetime.now()
 
